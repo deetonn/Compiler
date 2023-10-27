71359d47668bd284cea9dd9df3b6cb71f0b3563b
initializedd <bertzhallows@gmail.com>
2023-10-28 00:33:10 +0100

    Preprocessor/Lexing: Moved definitions to .cpp files

--- src/preprocessor/lexing/lexer.hpp
+++ src/preprocessor/lexing/lexer.hpp
@@ -3,63 +3,17 @@
 
 #include "../../common/common.hpp"
 
-#include <cstddef>
 #include <string>
-#include <sstream>
-#include <fstream>
 
 PREPROCESSOR_API_BEGIN
 
-static const std::string include = "#include";
+[[nodiscard]]
+auto copy_file_contents(const std::string& path) -> std::string;
 
-auto copy_file(const std::string& path) -> std::string {
-    std::fstream file(path);
-    std::istringstream iss;
-    iss >> file.rdbuf();
+[[nodiscard]]
+auto process_include(const std::string& line) -> bool;
 
-    return iss.str();
-}
-
-auto process_include(const std::string& line) -> bool {
-    bool found_opening, found_closed {};
-    std::string file;
-
-    for (std::size_t i = 0; i < line.length(); ++i) {
-        if (line[i] == ' ')
-            continue; // ignore white space
-
-        if (!found_opening) {
-            if (line[i] != '<')
-                return false; // error
-        }
-
-        auto pos = line.find(">", i);
-        if (pos == std::string::npos) {
-            return false; // error
-        }
-
-        file = line.substr(++i, pos);
-        copy_file(file);
-
-        return true;
-    }
-
-    return false;
-}
-
-auto preprocess(const std::string& path) -> void {
-    std::fstream file(path);
-    
-    while (!file.eof()) {
-        std::string line;
-        std::getline(file, line);
-
-        if (auto pos = line.find("#include") != std::string::npos) {
-           process_include(line.substr(pos + include.length())); 
-        }
-    }
-
-}
+auto preprocess(const std::string& path) -> void;
 
 PREPROCESSOR_API_END
 
