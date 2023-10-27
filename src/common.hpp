bc17632b77d5349c6bbeb3a133b48b7ee133bc9c
Deeton <deetonrushton1@outlook.com>
2023-10-27 19:34:16 +0100

    Add common file for macros that are globally used.

---
+++ src/common.hpp
@@ -0,0 +1,13 @@
+
+#ifndef COMPILER_COMMON_HPP
+
+#define COMPILER_API_BEGIN namespace compiler
+#define COMPILER_API_END }
+
+#define PREPROCESSOR_API_BEGIN namespace preprocessor
+#define PREPROCESSOR_API_END }
+
+#define COMPILER_API
+
+#define COMPILER_COMMON_HPP
+#endif // !COMPILER_COMMON_HPP
