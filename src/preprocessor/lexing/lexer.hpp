635317531aa33f1d8cc2cf76e7b4f8d154b2588d
initializedd <bertzhallows@gmail.com>
2023-10-27 21:39:59 +0100

    Preprocessor/Lexer: Added functionality - Added preprocess function - Added process_include function - Added copy_file function

--- src/preprocessor/lexing/lexer.hpp
+++ src/preprocessor/lexing/lexer.hpp
@@ -1,10 +1,66 @@
 #ifndef PREPROCESSOR_LEXER_HPP
 #define PREPROCESSOR_LEXER_HPP
 
-#include <optional>
+#include "../../common.hpp"
 
-namespace preprocessor {
-auto test_func() -> std::optional<int> { return std::nullopt; }
-} // namespace preprocessor
+#include <cstddef>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+PREPROCESSOR_API_BEGIN
+
+static const std::string include = "#include";
+
+auto copy_file(const std::string& path) -> std::string {
+    std::fstream file(path);
+    std::istringstream iss;
+    iss >> file.rdbuf();
+
+    return iss.str();
+}
+
+auto process_include(const std::string& line) -> bool {
+    bool found_opening, found_closed {};
+    std::string file;
+
+    for (std::size_t i = 0; i < line.length(); ++i) {
+        if (line[i] == ' ')
+            continue; // ignore white space
+
+        if (!found_opening) {
+            if (line[i] != '<')
+                return false; // error
+        }
+
+        auto pos = line.find(">", i);
+        if (pos == std::string::npos) {
+            return false; // error
+        }
+
+        file = line.substr(++i, pos);
+        copy_file(file);
+
+        return true;
+    }
+
+    return false;
+}
+
+auto preprocess(const std::string& path) -> void {
+    std::fstream file(path);
+    
+    while (!file.eof()) {
+        std::string line;
+        std::getline(file, line);
+
+        if (auto pos = line.find("#include") != std::string::npos) {
+           process_include(line.substr(pos + include.length())); 
+        }
+    }
+
+}
+
+PREPROCESSOR_API_END
 
 #endif // !PREPROCESSOR_LEXER_HPP
