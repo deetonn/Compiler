2973a72c1348b7e63d22244c381bbe67c5894d00
Deeton <deetonrushton1@outlook.com>
2023-10-28 10:49:29 +0100

    Common: Add TODO() for when you dont want to do something yet.

--- src/common/common.hpp
+++ src/common/common.hpp
@@ -1,6 +1,9 @@
 
 #ifndef COMPILER_COMMON_HPP
 
+#include <cstdlib>
+#include <iostream>
+
 #define COMPILER_API_BEGIN namespace compiler {
 #define COMPILER_API_END }
 
@@ -8,6 +11,8 @@
 #define PREPROCESSOR_API_END }
 
 #define COMPILER_API
+
+#define TODO(msg) std::cerr << "TODO: " msg << '\n'; std::exit(-1)
 
 #define COMPILER_COMMON_HPP
 #endif // !COMPILER_COMMON_HPP
