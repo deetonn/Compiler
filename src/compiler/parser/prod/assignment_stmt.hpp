f03d419b15cc064cd2a866e288576317da281529
Deeton <deetonrushton1@outlook.com>
2023-10-30 14:15:17 +0000

    Compiler/Parser: Add `assignment_statement`.
    This is for actual variable declarations.

--- src/compiler/parser/prod/assignment_stmt.hpp
+++ src/compiler/parser/prod/assignment_stmt.hpp
@@ -6,11 +6,51 @@
 #include "node.hpp"
 #include "../visitor.hpp"
 
+#include "../../types.hpp"
+
 COMPILER_API_BEGIN
 
 class assignment_statement : public statement {
+private:
+  type_information m_type_info;
+  identifier m_identifier;
+  source_location m_source_location;
+  std::optional<std::unique_ptr<expression>> m_expr;
 public:
-      
+  COMPILER_API inline explicit assignment_statement(
+      const type_information& type,
+      const identifier& identifier,
+      const source_location& location,
+      std::optional<std::unique_ptr<expression>> expr = std::nullopt          
+  )
+    : m_type_info(type)
+    , m_identifier(identifier)
+    , m_source_location(location)
+  {
+      if (expr.has_value()) {
+          m_expr = std::move(expr.value());            
+      }
+  }
+
+  inline virtual void accept(ast_visitor& visitor) noexcept {
+      return visitor.visit_assignment_statement(*this);          
+  } 
+
+  inline virtual const source_location& location() const noexcept {
+      return m_source_location;          
+  }
+
+  inline const type_information& type() const noexcept {
+      return m_type_info;          
+  }
+
+  inline const identifier& identifier() const noexcept {
+      return m_identifier;
+  }
+
+  inline bool has_expr() const noexcept { return m_expr.has_value(); }
+  inline expression* expr() noexcept { return m_expr->get(); }
+  inline const expression* expr() const noexcept { return m_expr->get(); }
 };
 
 COMPILER_API_END
