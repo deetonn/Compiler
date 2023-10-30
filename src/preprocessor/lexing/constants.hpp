2e8edac4e0cdfc61219fff4e90df784d7d03be16
initializedd <bertzhallows@gmail.com>
2023-10-30 02:22:03 +0000

    Initial commit

---
+++ src/preprocessor/lexing/constants.hpp
@@ -0,0 +1,54 @@
+#ifndef PREPROCESSOR_LEXING_CONSTANTS_HPP
+#define PREPROCESSOR_LEXING_CONSTANTS_HPP
+
+#include "../../common/common.hpp"
+#include "token_type.hpp"
+
+#include <unordered_map>
+
+PREPROCESSOR_API_BEGIN
+
+// C preprocessor tokens
+// TODO: some tokens may be missing, and some may not be part of the standard
+static const std::unordered_map<std::string, token_type> tokens = {
+        // standard preprocessor operators
+      { "#",                    token_type::HASHTAG },
+
+        // standard preprocessor directives
+      { "include",              token_type::INCLUDE },
+      { "define",               token_type::DEFINE },
+      { "if",                   token_type::IF },
+      { "elif",                 token_type::ELIF },
+      { "else",                 token_type::ELSE },
+      { "endif",                token_type::ENDIF },
+      { "ifdef",                token_type::IFDEF },
+      { "ifndef",               token_type::IFNDEF },
+      { "undef",                token_type::UNDEF },
+      { "pragma",               token_type::PRAGMA },
+      { "using",                token_type::USING },
+      { "error",                token_type::ERROR },
+      { "warning",              token_type::WARNING },
+      { "elifdef",              token_type::ELIFDEF },
+      { "elifndef",             token_type::ELIFNDEF },
+      { "embed",                token_type::EMBED },
+
+        // standard predefined identifier
+      { "__func__",             token_type::FUNC },
+
+        // standard predefined macros
+      { "__DATE__",             token_type::DATE },
+      { "__TIME__",             token_type::TIME },
+      { "__FILE__",             token_type::FILE },
+      { "__LINE__",             token_type::LINE },
+      { "__STDC__",             token_type::STDC },
+      { "__STDC_HOSTED__",      token_type::STDC_HOSTED },
+      { "__STDC_NO_ATOMICS__",  token_type::STDC_NO_ATOMICS },
+      { "__STDC_NO_COMPLEX__",  token_type::STDC_NO_COMPLEX },
+      { "__STDC_NO_THREADS__",  token_type::STDC_NO_THREADS },
+      { "__STDC_NO_VLA__",      token_type::STDC_NO_VLA },
+      { "__STDC_VERSION__",     token_type::STDC_VERSION }
+};
+
+PREPROCESSOR_API_END
+
+#endif // !PREPROCESSOR_LEXING_CONSTANTS_HPP
---
+++ src/preprocessor/lexing/token_type.hpp
@@ -0,0 +1,87 @@
+#ifndef PREPROCESSOR_LEXING_TOKEN_TYPE_HPP
+#define PREPROCESSOR_LEXING_TOKEN_TYPE_HPP
+
+#include "../../common/common.hpp"
+
+#include <string>
+
+PREPROCESSOR_API_BEGIN
+
+enum class token_type {
+    // standard preprocessor operators
+    HASHTAG,
+
+    // standard preprocessor directives
+    INCLUDE,
+    DEFINE,
+    IF,
+    ELIF,
+    ELSE,
+    ENDIF,
+    IFDEF,
+    IFNDEF,
+    UNDEF,
+    PRAGMA,
+    USING,
+    ERROR,
+    WARNING,
+    ELIFDEF,
+    ELIFNDEF,
+    EMBED,
+
+    // standard predefined identifier
+    FUNC,
+
+    // standard predefined macros
+    DATE,
+    TIME,
+    FILE,
+    LINE,
+    STDC,
+    STDC_HOSTED,
+    STDC_NO_ATOMICS,
+    STDC_NO_COMPLEX,
+    STDC_NO_THREADS,
+    STDC_NO_VLA,
+    STDC_VERSION
+};
+
+inline std::string token_type_to_string(const token_type type) {
+    switch (type) {
+        case token_type::HASHTAG:           return "HASHTAG";
+        case token_type::INCLUDE:           return "INCLUDE";
+        case token_type::DEFINE:            return "DEFINE";
+        case token_type::IF:                return "IF";
+        case token_type::ELIF:              return "ELIF";
+        case token_type::ELSE:              return "ELSE";
+        case token_type::ENDIF:             return "ENDIF";
+        case token_type::IFDEF:             return "IFDEF";
+        case token_type::IFNDEF:            return "IFNDEF";
+        case token_type::UNDEF:             return "UNDEF";
+        case token_type::PRAGMA:            return "PRAGMA";
+        case token_type::USING:             return "USING";
+        case token_type::ERROR:             return "ERROR";
+        case token_type::WARNING:           return "WARNING";
+        case token_type::ELIFDEF:           return "ELIFDEF";
+        case token_type::ELIFNDEF:          return "ELIFNDEF";
+        case token_type::EMBED:             return "EMBED";
+        case token_type::FUNC:              return "FUNC";
+        case token_type::DATE:              return "DATE";
+        case token_type::TIME:              return "TIME";
+        case token_type::FILE:              return "FILE";
+        case token_type::LINE:              return "LINE";
+        case token_type::STDC:              return "STDC";
+        case token_type::STDC_HOSTED:       return "STDC_HOSTED";
+        case token_type::STDC_NO_ATOMICS:   return "STDC_NO_ATOMICS";
+        case token_type::STDC_NO_COMPLEX:   return "STDC_NO_COMPLEX";
+        case token_type::STDC_NO_THREADS:   return "STDC_NO_THREADS";
+        case token_type::STDC_NO_VLA:       return "STDC_NO_VLA";
+        case token_type::STDC_VERSION:      return "STDC_VERSION";
+
+        default:                            return "UNKNOWN";
+    }
+}
+
+PREPROCESSOR_API_END
+
+#endif // !PREPROCESSOR_LEXING_TOKEN_TYPE_HPP
